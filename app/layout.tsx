90f34200817eb133fb9fb3ba48a1daf6f579b182
olga-evdokimova <pavedom13@gmail.com>
2023-09-15 20:45:43 +0300

    *

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,9 +3,7 @@ import "./null.scss";
 import "./style.scss";
 import type { Metadata } from "next";
 import { Montserrat } from "next/font/google";
-import Footer from "./components/Footer/Footer";
-import Header from "./components/Header/Header";
-import Button from "./components/Button/Button";
+
 import ScrollToTopButton from "./components/ScrollToTopButton/ScrollToTopButton";
 
 export const montserrat = Montserrat({
@@ -28,13 +26,13 @@ export default function RootLayout({
     <html lang="en">
       <body className={montserrat.className}>
         <div className="wrapper">
-          <Header />
+ 
           <main>
             {children}
-            <Button />
+   
             <ScrollToTopButton />
           </main>
-          <Footer />
+      
         </div>
       </body>
     </html>
